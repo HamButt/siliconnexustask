f8d785a15206b4f44b09a832abec50b6cc72158a
Hammad butt <98612242+HamButt@users.noreply.github.com>
2024-02-04 18:03:57 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(bodyParser.json())
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: 'https://siliconnexustask-frontend.vercel.app',
   methods: ["GET", "POST", "DELETE", "PUT"],
   credentials: true
 };
