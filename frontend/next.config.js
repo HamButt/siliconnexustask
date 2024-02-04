6aba56b6ffcb40ab18180b5ef5d2fb6fd529c97d
Hammad butt <98612242+HamButt@users.noreply.github.com>
2024-02-04 18:34:58 +0500

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -9,7 +9,7 @@ module.exports = (phase) => {
     reactStrictMode: false,
     images: { unoptimized: true },
     env: {
-      baseUrl: isDev ? 'http://localhost:5000' : 'https://siliconnexustask-backend.vercel.app',
+      baseUrl: isDev ? 'http://localhost:5000' : 'https://redemy.com',
     },
     // swcMinify: false,
   }
