0c82e1bb4acde1ba7392913b1ea0c7c0261e9534
Hammad butt <98612242+HamButt@users.noreply.github.com>
2024-02-04 17:42:51 +0500

    Update config.js

--- backend/database/config.js
+++ backend/database/config.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const monogoDb = mongoose.connect('mongodb://localhost:27017/remedy')
+const monogoDb = mongoose.connect('mongodb+srv://hammadbutt02123:8pEsm2Ct3SIgrtAo@remedy.a6g0jlv.mongodb.net/?retryWrites=true&w=majority')
 .then(() => console.log('Database Connected!'))
 .catch((err) => console.log("Error: " + err))
 
